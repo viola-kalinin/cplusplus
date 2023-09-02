7b81e46677fb9560e2a971787d90475d7b3f3c4b
Viola Kalinin <36274010+viola-kalinin@users.noreply.github.com>
2023-09-02 12:41:57 -0700

    update with notes

--- help.cpp
+++ help.cpp
@@ -1,9 +1,10 @@
 // Your First C++ Program
 
-#include <iostream>
-using namespace std;
+#include <iostream> //Before compilation, these directives are interpreted to determine which libraries or code to include. This is for input/ output
+using namespace std; //allows use of names of objects and variables from the standard library
 
-int main() {
-    cout << "Hello World!" <<endl;
-    return 0;
+int main() { //All C++ programs begin with the execution of this function, regardless of its location in the source code
+    cout << "Hello World!" << endl; //cout is the character output device. << is the insertion operator.
+    //if not using namespace std, we would use std::cout and std::endl
+    return 0; //because main is defined as returning int, we return 0 to signify that the program ran without errors
 }
