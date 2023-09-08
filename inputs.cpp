32de0bb7ebd57dee7fd168dcc029dee56498084b
Viola Kalinin <36274010+viola-kalinin@users.noreply.github.com>
2023-09-07 18:40:10 -0700

    first commit

---
+++ inputs.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main(){
+  int a = 24;
+  int b;
+  string s;
+  getline(cin,s);
+  stringstream my_stream(s);
+  my_stream >> b;
+  cout << a+b << endl;
+  cout << my_stream.fail() << endl;
+  return 0;
+}
+
+
+
+
+
+
+
+
