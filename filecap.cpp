47fe22cbe711ef55c9290ea7b098e37127c0c042
Viola Kalinin <36274010+viola-kalinin@users.noreply.github.com>
2023-10-08 15:33:50 -0700

    update

--- filecap.cpp
+++ filecap.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main(int argc, char * argv[]){
     ifstream fin;
-    ofstream out_file;
+    ofstream fout;
     try {
         fin.open("test.txt"); 
         if(!fin) {
@@ -16,22 +16,22 @@ int main(int argc, char * argv[]){
     catch(string err){
         cout<<err; 
     }
-    out_file.open(argv[1]); 
+    fout.open(argv[1]); 
     string line;
     while(getline(fin,line)){
         for (char a: line){
             if (isupper(a)){
-                out_file << char(tolower(a)); 
+                fout << char(tolower(a)); 
             }
             else if(islower(a)){ 
-                out_file << char(toupper(a));
+                fout << char(toupper(a));
             }
             else{
-                out_file << a;
+                fout << a;
             }
         }
-        out_file << endl;
+        fout << endl;
     }
     fin.close();
-    out_file.close();
+    fout.close();
 }
